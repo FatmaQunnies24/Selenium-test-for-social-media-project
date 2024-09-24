5790b36ce9bcebbd08a5a6ff3cdfa0f64a864fc9
FatmaQunnies24 <fatima.n.qunnies@gmail.com>
2024-09-24 12:26:43 +0300

    first commit

---
+++ src/test/java/q/example/q/QApplicationTests.java
@@ -0,0 +1,13 @@
+package q.example.q;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
