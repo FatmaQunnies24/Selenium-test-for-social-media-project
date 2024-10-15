892b18296588e1f9b69dfa0e52482b75b8021bb5
FatmaQunnies24 <fatima.n.qunnies@gmail.com>
2024-10-15 13:21:53 +0300

    ddd

--- src/main/java/q/example/q/InvalidLoginTest.java
+++ src/main/java/q/example/q/InvalidLoginTest.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
-// import org.junit.Assert;
 public class InvalidLoginTest {
 
     public static void main(String[] args) {
@@ -16,29 +15,22 @@ public class InvalidLoginTest {
         WebDriver driver = new ChromeDriver();
 
         try {
-            // تكبير النافذة
             driver.manage().window().maximize();
             
-            // الانتقال إلى موقع saucedemo
             driver.get("https://www.saucedemo.com/");
             
-            // العثور على حقول اسم المستخدم وكلمة المرور
             WebElement usernameField = driver.findElement(By.id("user-name"));
             WebElement passwordField = driver.findElement(By.id("password"));
             
-            // إدخال معلومات غير صحيحة
-            usernameField.sendKeys("wrong_user");
+            usernameField.sendKeys("standard_user");
             passwordField.sendKeys("wrong_password");
             
-            // الضغط على زر تسجيل الدخول
             WebElement loginButton = driver.findElement(By.id("login-button"));
             loginButton.click();
             
-            // انتظار ظهور رسالة الخطأ
 WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
             WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h3[data-test='error']")));
             
-            // التحقق من ظهور رسالة الخطأ
             String expectedErrorMessage = "Epic sadface: Username and password do not match any user in this service";
             // Assert.assertEquals(expectedErrorMessage, errorMessage.getText());
             String actualErrorMessage = errorMessage.getText();
@@ -51,7 +43,6 @@ if (!expectedErrorMessage.equals(actualErrorMessage)) {
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            // إغلاق المتصفح بعد الانتهاء
             driver.quit();
         }
     }
